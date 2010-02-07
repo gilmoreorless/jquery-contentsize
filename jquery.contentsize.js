4f5de03ec451f7eefa2344044ad3a36fb58463fb
Gilmore Davidson <gilmoreorless@gmail.com>
2010-02-07 21:03:53 +1100

    Added quick calculation option that uses last element

--- jquery.contentsize.js
+++ jquery.contentsize.js
@@ -11,17 +11,19 @@
  */
 (function($){
 	$.each(['Width', 'Height'], function (i, name) {
-		$.fn['content' + name] = function () {
+		$.fn['content' + name] = function (quick) {
 			var pos = name == 'Width' ? 'left' : 'top';
 			if (!this[0]) {
 				return null;
 			}
-			var $this = $(this),
-				$children = $this.children(),
+			var $this = this.eq(0),
+				filter = quick === true ? ':last' : '',
+				$children = $this.children(filter),
 				total = 0;
 			if (!$children.length) {
 				return total;
 			}
+
 			$children.each(function () {
 				var $child = $(this),
 					val = $child.position()[pos] + $child['outer' + name](true);
